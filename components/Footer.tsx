bc0728d8ca5d6ac13a6ecdd8abe9712e3d89104d
Watanabe Kei <watanabe@un-t.com>
2022-10-24 14:35:59 +0900

    fix: Footer design

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,10 +4,10 @@ export const Footer: FC = () => {
   const year = new Date().getFullYear();
   return (
     <footer className='mt-[60px] text-[4rem]'>
-      <p>
+      <p className='leading-[0.7em]'>
         <small>&copy; {year} p_toro All Rights Reserved.</small>
       </p>
-      <ul className='mt-[10px] flex flex-wrap gap-x-[1rem] text-[2rem]'>
+      <ul className='mt-[12px] flex flex-wrap gap-x-[1rem] text-[2rem]'>
         <li>Next.js</li>
         <li>TypeScript</li>
         <li>Tailwind</li>
